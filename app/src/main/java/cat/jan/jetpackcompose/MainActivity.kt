f0a8e9660697646149bcf15c54d78dc3c06c2b47
Jan Candalija <jcandalija.dam@institutcampalans.net>
2025-03-31 16:57:05 +0200

    Canvis pràctiva final de jetpack compose

--- app/src/main/java/cat/jan/jetpackcompose/MainActivity.kt
+++ app/src/main/java/cat/jan/jetpackcompose/MainActivity.kt
@@ -1,17 +1,43 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package cat.jan.jetpackcompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -30,18 +56,24 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun App() {
+    var puntsCSharp by rememberSaveable { mutableStateOf(0) }
+    var puntsJava by rememberSaveable { mutableStateOf(0) }
+    var puntsKotlin by rememberSaveable { mutableStateOf(0) }
+
     MaterialTheme (
     ) {
         Scaffold(
+            topBar = { topBar() },
             modifier = Modifier
                 .fillMaxSize()
                 .background(color = Color(0xFF24292e))
-                .padding(start = 30.dp, end = 30.dp)
         ) { padding ->
             LazyColumn (
                 modifier = Modifier
                     .fillMaxSize()
                     .background(Color(0xFF24292e))
+                    .padding(padding)
+                    .padding(start = 30.dp, end = 30.dp)
             ) {
                 item {
                     Image(
@@ -51,6 +83,14 @@ fun App() {
                         painter = painterResource(id = R.drawable.institut_rafael_campalans),
                         contentDescription = "Imatge del curriculum"
                     )
+
+                    Column(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        filaConeixements("C#", puntsCSharp, { puntsCSharp++ }, { puntsCSharp = 0} )
+                        filaConeixements("Java", puntsJava, { puntsJava++ }, { puntsJava = 0} )
+                        filaConeixements("Kotlin", puntsKotlin, { puntsKotlin++ }, { puntsKotlin = 0} )
+                    }
 
                     Card(
                         modifier = Modifier
@@ -184,6 +224,84 @@ fun App() {
 
 
             }
+        }
+    }
+}
+
+@Composable
+fun topBar() {
+    val context = LocalContext.current
+    TopAppBar(
+        title = { Text(
+            text = "Jan Candalija",
+            color = Color.White,
+            modifier = Modifier.padding(start = 20.dp)
+        ) },
+        modifier = Modifier.background(Color.Black),
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.Black
+        ),
+        navigationIcon = {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_home_24),
+                contentDescription = "Inici",
+                modifier = Modifier
+                    .clickable {
+                        Toast.makeText(context, "Funcionaitat en contrucció", Toast.LENGTH_LONG).show()
+                    }
+                    .padding(start = 20.dp)
+                    .background(Color.White)
+            )
+        }
+    )
+}
+
+@Composable
+fun filaConeixements(llenguatge: String, punts: Int, augmentarPunts: () -> Unit, netejar: () -> Unit) {
+    Row (
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(
+            fontSize = 20.sp,
+            text = llenguatge,
+            color = Color.White,
+            textAlign = TextAlign.Start,
+            modifier = Modifier
+                .align(alignment = Alignment.CenterVertically)
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Text(
+                fontSize = 20.sp,
+                text = punts.toString(),
+                color = Color.White,
+                modifier = Modifier
+                    .align(alignment = Alignment.CenterVertically)
+                    .padding(end = 60.dp)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.ic_add),
+                contentDescription = "Guarda",
+                modifier = Modifier
+                    .clickable {
+                        augmentarPunts()
+                    }
+                    .height(45.dp)
+                    .width(45.dp)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.ic_clear),
+                contentDescription = "Netejar",
+                modifier = Modifier
+                    .clickable {
+                        netejar()
+                    }
+                    .height(45.dp)
+                    .width(45.dp)
+            )
         }
     }
 }
